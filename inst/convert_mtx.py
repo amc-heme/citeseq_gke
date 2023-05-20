73015e7d879a584cf3c8d866449ce69216bf36ad
Austin Gillen <4809242+agillen@users.noreply.github.com>
2023-05-19 20:45:06 -0600

    replace anndata with scanpy for easier 10X mtx conversion

--- inst/convert_mtx.py
+++ inst/convert_mtx.py
@@ -1,8 +1,8 @@
-import anndata as ad
+import scanpy as sc
 import sys
 
 print("Reading matrix")
-adata = ad.read_mtx(sys.argv[1])
+adata = sc.read_10x_mtx(sys.argv[1])
 print("Writing h5ad")
 adata.write_h5ad(str(sys.argv[2]))
 print("Done!")
